8fbc9fd6a41b6dcaf292580136dce87a186678c3
Thanos <stantzouris.thanos@gmail.com>
2021-03-17 23:10:46 +0200

    Episode 2 - Initial commit and deisgn

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans text-gray-900 text-sm bg-gray-100">
+        <header class="flex items-center justify-between px-8 py-4">
+            <a href="#"><img src="{{ asset('img/logo.svg') }}" alt=""></a>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                    <div class="px-6 py-4 sm:block">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log out') }}
+                                </a>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+                <a href="#">
+                    <img    class="w-10 h-10 rounded-full"
+                            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar">
+                </a>
+            </div>
+        </header>
+
+        <main class="container mx-auto max-w-custom flex">
+            <div class="w-70 mr-5" >
+                Add an idea form goes here. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo laudantium, ab est eos, magni eveniet tempore nobis ut sapiente adipisci eligendi commodi!
+            </div>
+            <div class="w-175" >
+                <nav class="flex items-center justify-between text-xs">
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li><a href="" class="border-b-4 pb-3 border-blue-500">All ideas (87)</a></li>
+                        <li><a href="" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500">Considering (6)</a></li>
+                        <li><a href="" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500">in progress (1)</a></li>
+                    </ul>
+
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li><a href="" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500">Implemented (10)</a></li>
+                        <li><a href="" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500">Closed (55)</a></li>
+                    </ul>
+                </nav>
+
+                <div class="mt-8">
+                    {{ $slot }}
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            maxWidth: {
+                custom: '62.5rem',
+            },
+            spacing: {
+                70: '17.5rem',
+                175: '43.75rem',
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
